56e4d47c251cda3d7b1070a49a00493f73fb8cd8
Martha Julia <al02954101@tecmilenio.mx>
2021-04-09 22:09:28 -0700

    actualmente actualizando un comentario en mi archivo

--- ejemploS3.js
+++ ejemploS3.js
@@ -1,1 +1,3 @@
+//Este fue mi primer archivo subido a un repositorio
 console.log("Este es una rchivo que estaré subiendo al miSegundoRepositorio para la sesión 1.3 del módulo 1");
+//este es un comentario agregado para probar el subir una nueva version con git bash
